af304815dca78798b344ab7a3ff43df6a8d38f69
Eray Hanoglu <e.hanoglu@panates.com>
2018-03-05 12:40:44 +0300

    First release

---
+++ test/tests.js
@@ -0,0 +1,56 @@
+/* eslint-disable */
+const assert = require('assert');
+const {walkdir, walkdirSync} = require('../');
+
+describe('Walkdir', function() {
+
+  it('should walk directory async', function(done) {
+    let ok = 0;
+    walkdir('./', function(f, stats) {
+      if (stats.isDirectory() && f === 'lib')
+        ok++;
+      if (!stats.isDirectory() && f === 'README.md')
+        ok++;
+    }, function() {
+      assert(ok === 2, 'Failed');
+      done();
+    });
+  });
+
+  it('should walk directory sync', function() {
+    let ok = 0;
+    walkdirSync('./', function(f, stats) {
+      if (stats.isDirectory() && f === 'lib')
+        ok++;
+      if (!stats.isDirectory() && f === 'README.md')
+        ok++;
+    });
+    assert(ok === 2, 'Failed');
+  });
+
+  it('should ignore directory async', function(done) {
+    let ok = 1;
+    walkdir('./', function(f, stats) {
+      if (stats.isDirectory() && f === 'test')
+        return false;
+      if (!stats.isDirectory() && f.startsWith('test/'))
+        ok = 0;
+    }, function() {
+      assert(ok, 'Failed');
+      done();
+    });
+  });
+
+  it('should ignore directory sync', function() {
+    let ok = 1;
+    walkdir('./', function(f, stats) {
+      if (stats.isDirectory() && f === 'test')
+        return false;
+      if (!stats.isDirectory() && f.startsWith('test/'))
+        ok = 0;
+    });
+    assert(ok, 'Failed');
+  });
+
+});
+
